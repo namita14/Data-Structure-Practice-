c9c942f3f614f7b1b945d515a5baf476a9e5aadc
Namita Maheshwari <68988503+namita14@users.noreply.github.com>
2022-01-29 20:29:04 +0530

    Add files via upload

---
+++ IntersectionArray.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        unordered_set<int>s1;
+        unordered_set<int>s2;
+        for(int i=0;i<nums1.size();i++)
+            s1.insert(nums1[i]);
+          for(int i=0;i<nums2.size();i++)
+            s2.insert(nums2[i]);
+        for(auto i=s1.begin();i!=s1.end();i++)
+            if(s2.find(*i)!=s2.end())
+                ans.push_back(*i);
+        return ans;
+    }
---
+++ UnionArray.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int doUnion(int a[], int n, int b[], int m)  {
+        //code here
+        unordered_map<int, int> mp;
+        for(int i=0;i<n;i++){
+            mp[a[i]]++;
+        }
+        
+        for(int j=0;j<m;j++){
+            mp[b[j]]++;
+        }
+    return mp.size();
+}
+
+
+int main(){
+    int n, m;
+    cin>>n>>m;
+    int a[n], b[m];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+
+    for(int j=0;j<m;i++){
+        cin>>b[j];
+    }
+
+    cout<<doUnion(a,n,b,m)<<endl;
+}
